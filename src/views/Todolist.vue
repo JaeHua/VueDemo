26dd113ec0624da279ccfff6f16116ed3acbdc48
JJH529529 <126366914+JJH529529@users.noreply.github.com>
2024-05-13 13:44:36 +0800

    step12.todolist页面逻辑构造

--- src/views/Todolist.vue
+++ src/views/Todolist.vue
@@ -1,5 +1,14 @@
 <script setup>
-import {onBeforeUnmount, onMounted} from "vue";
+import {onBeforeUnmount, onMounted, reactive, ref} from "vue";
+import {useStore} from "vuex";
+const store = useStore()
+
+//todo
+const data = reactive({
+  title:'',
+  status:0,
+  telephone:store.state.userModule.userInfo.telephone,
+})
 
 onMounted(() => {
   document
@@ -42,7 +51,7 @@ onMounted(() => {
       </div>
   </el-header>
     <el-main class="parent-container">
-      <input type="text"  class="add-content" placeholder="新增待办事项..."/>
+      <input type="text"  class="add-content" v-model="data.title" placeholder="新增待办事项..."/>
       <button class="btn submit-btn" type="button" @click="">提交</button>
     </el-main>
     <el-main  class="parent-container">
@@ -52,15 +61,38 @@ onMounted(() => {
           今日事今日毕，勿将今事待明日! 🤏🤏
         </div>
       </div>
-      <ul class="empty-tips">
-        <li> 添加你的第一个代办事项！📝</li>
-        <li>食用方法💡：</li>
-        <li>✔️ 所有提交操作支持 Enter 回车键提交~</li>
-        <li>✔️ 拖拽 Todo 上下移动可排序(仅支持 PC)~</li>
-        <li>✔️ 双击已有的 Todo 可进行编辑~</li>
-        <li>✔️ 右侧的小窗口是快捷操作哦~</li>
-        <li>🔒 还在搭建...</li></ul>
+<!--      <ul class="empty-tips">-->
+<!--        <li> 添加你的第一个代办事项！📝</li>-->
+<!--        <li>食用方法💡：</li>-->
+<!--        <li>✔️ 所有提交操作支持 Enter 回车键提交~</li>-->
+<!--        <li>✔️ 拖拽 Todo 上下移动可排序(仅支持 PC)~</li>-->
+<!--        <li>✔️ 双击已有的 Todo 可进行编辑~</li>-->
+<!--        <li>✔️ 右侧的小窗口是快捷操作哦~</li>-->
+<!--        <li>🔒 还在搭建...</li></ul>-->
+<!--        <ul class="empty-tips">-->
+<!--        </ul>-->
+      <ul class="todo-list">
+        <li  class='todo-item'>
+          <div class="todo-content">{{data.title}}</div>
+          <div class="todo-btn btn-finish"></div>
 
+          <div class="todo-btn btn-delete">
+            <img
+              src="@/assets/x.svg"
+              style="width: 75% "
+          /></div>
+        </li>
+        <li  class='todo-item'>
+          <div class="todo-content">吃饭</div>
+          <div class="todo-btn btn-finish"></div>
+
+          <div class="todo-btn btn-delete">
+            <img
+                src="@/assets/x.svg"
+                style="width: 75% "
+            /></div>
+        </li>
+      </ul>
     </div>
     </el-main>
     <div  class="side-bar" @click="$router.replace('home')" style="cursor: pointer;">
@@ -71,6 +103,9 @@ onMounted(() => {
 </template>
 
 <style scoped>
+
+
+
 .container{
   margin-top: 20px;
 }
@@ -83,17 +118,24 @@ onMounted(() => {
   align-items: center;
 
 }
+
 .add-content {
   margin-top: 15px;
-  width: 45%;
+  width: 40%;
   font-size: 18px;
   cursor: text;
   border: 2px solid #33322E;
-  border-radius: 12px 0 0 12px;
   height: 55px;
   line-height: 64px;
   text-indent: 12px;
+  transition: all 0.35s;
+  border-radius: 12px 0 0 12px;
   box-shadow: 4px 4px 0 #33322E;
+}
+
+.submit-btn:hover,
+.add-content:hover {
+  box-shadow: 0 0 0 0 #333;
 }
 .submit-btn {
 
@@ -114,6 +156,7 @@ onMounted(() => {
   text-indent: 12px;
   box-shadow: 4px 4px 0 #33322E;
 }
+
 .btn {
   border: 2px solid #33322E;
   text-align: center;
@@ -130,8 +173,8 @@ onMounted(() => {
   padding-top: 0;
   transition: all .5s ease;
   max-width: 100%;
-  width: 53%;
-
+  width: 48%;
+  height: 350px;
   overflow: hidden;
 }
 .bar-message {
@@ -188,7 +231,89 @@ ul, li {
   z-index: 999;
   cursor: pointer;
 }
+.todo-list {
+  padding: 32px 36px 36px;
+  min-height: 381px;
+  transition: all .5s ease;
 
+}
+.todo-content {
+  border: 2px solid #33322E;
+  box-shadow: 4px 4px 0 #33322E;
+
+  background: #F9F3E5;
+  border-radius: 12px;
+  width: 100%;
+  position: relative;
+  box-sizing: border-box;
+  display: block;
+  line-height: 1;
+  overflow-wrap: break-word;
+  padding: 20px 56px;
+  cursor: pointer;
+  min-height: 60px;
+  transition: all 0.35s;
+}
+.todo-content:hover{
+  box-shadow: 0 0 0 0 #33322E;
+
+}
+.btn-finish {
+  position: relative;
+  margin-top: -55px;
+  left: 15px;
+  top: 10px;
+  width: 25px;
+  height: 25px;
+  border-radius: 20px;
+  z-index: 9999;
+}
+.btn-finish:hover{
+  box-shadow: -4px 4px 0 #33322E;
+  transform: translate(5px, -5px);
+
+}
+.todo-btn {
+  background: none;
+  display: block;
+  cursor: pointer;
+  border: 2px solid #33322E;
+  transition: all .2s;
+  background: #fff;
+}
+.todo-btn:hover {
+  background: #8cd4cb;
+  border: 2px solid #33322E;
+
+
+}
+.todo-list li {
+  position: relative;
+  border-radius: 12px;
+  width: 100%;
+  transition: all .5s ease;
+  display: block;
+  margin-bottom: 18px;
+}
+.btn-delete {
+  position: relative;
+  background: #fff;
+  left: 525px;
+  top: -17px;
+  height: 25px;
+  width: 25px;
+  border-radius: 12px;
+  font-size: 13px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999;
+
+}
+.btn-delete:hover{
+  box-shadow: 4px 4px 0 #33322E;
+  transform: translate(-5px, -5px);
+}
 :root {
   --body-bg: #e8f6ff;
   --border-radius: 12px;
